218c9c8370f5e8d2ceb11715eaba1acfaa48c41f
조하명 <carrey@nexus.co.kr>
2019-11-28 02:00:55 +0900

    commit

--- random-chat/src/main/java/com/javaboja/controller/ChatController.java
+++ random-chat/src/main/java/com/javaboja/controller/ChatController.java
@@ -1,8 +1,16 @@
 package com.javaboja.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+//	@MessageMapping("/chat")
+//	@SendTo("/topic/chat")
+//	public Chat chat(Chat chat) throws Exception{
+//		log.info("chat");
+//		return new Chat(chat.getName(), chat.getMessage());
+//	}
 }
