2ed6e8d8df25acc6bac2ba7b0d7201ed9387f04d
조하명 <carrey@nexus.co.kr>
2020-03-26 01:41:53 +0900

    first commit

---
+++ Undefined_project/src/main/java/com/carrey/UndefinedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.carrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndefinedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndefinedProjectApplication.class, args);
+	}
+
+}
---
+++ Undefined_project/src/test/java/com/carrey/UndefinedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndefinedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
