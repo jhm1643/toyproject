f90ef4863bb5e201db2fc3754a708e41447f88d6
조하명 <carrey@nexus.co.kr>
2020-03-27 01:01:52 +0900

    commit

---
+++ Undefined_project/src/main/java/com/carrey/view/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.carrey.view;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{
+
+	public void addViewControllers(ViewControllerRegistry regi) {
+		regi.addViewController("/").setViewName("home");
+		regi.addViewController("/login").setViewName("login");
+	}
+}
